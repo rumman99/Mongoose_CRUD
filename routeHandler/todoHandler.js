d060d0475781065fefc74f7745e7279cbf47d711
rumman99 <tasnimrumman369@gmail.com>
2024-01-23 18:14:56 +0600

    crud done

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -8,12 +8,25 @@ const Task= new mongoose.model('Task', todoSchema);
 
 // All Todo Show
 router.get('/', async (req, res)=>{
+    try{
+        const allTask= await Task.find({}).select({_id: 0, __v: 0});
+        res.status(200).json({allTask})
+    }
+    catch(err){
+        res.status(500).json({error: "Something Wrong"})
+    }
 
 });
 
 // Get Single Todo by ID
 router.get('/:id', async (req, res)=>{
-
+    try{
+        const allTask= await Task.find({_id: req.params.id}).select({_id: 0, __v: 0});
+        res.status(200).json({allTask})
+    }
+    catch(err){
+        res.status(500).json({error: "Something Wrong"})
+    }
 });
 
 // Post a Single Todo 
@@ -33,7 +46,7 @@ router.post('/all', async (req, res)=>{
     // const newTask= new Task(req.body)
     try{
         await Task.insertMany(req.body);
-        res.status(200).json({message: "Successfully Store Data"})
+        res.status(200).json({message: "Successfully Store Multiple Data"})
     }
     catch(err){
         res.status(500).json({error: err})
@@ -45,7 +58,7 @@ router.put('/:id', async (req, res)=>{
     try{
         await Task.updateOne({_id: req.params.id}, {$set:{status: req.body.status}})
         // const result= await Task.findByIdAndUpdate({_id: req.params.id}, {$set:{status: req.body.status}}, {new: true})
-        res.status(200).json({message: "Successfully Store Data"})
+        res.status(200).json({message: "Successfully Update Data"})
         // console.log(result);
     }
     catch(err){
@@ -54,10 +67,10 @@ router.put('/:id', async (req, res)=>{
 });
 
 // Delete Single TOdo by Id
-router.delete('/all', async (req, res)=>{
+router.delete('/:id', async (req, res)=>{
     try{
-        
-        res.status(200).json({message: "Successfully Store Data"})
+        await Task.deleteOne({_id: req.params.id})
+        res.status(200).json({message: "Successfully Delete Data"})
     }
     catch(err){
         res.status(500).json({error: err})
